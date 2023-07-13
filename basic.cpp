52e6f0720fba7d22420a070901c4ebc20641ef97
meet <meetkoladiya0171@gmail.com>
2023-07-13 10:46:51 +0530

    13-07-23

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout<<"hello world";
+    cout<<"\nenter value a :";
+    cin>>a;
+    cout<<"\nenter value b :";
+    cin>>b;
+    cout<<"product is : "<<a*b;
+}
