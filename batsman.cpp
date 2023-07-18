fa2b44b6e7c35006df11939fdfde4aa7547cd126
meet <meetkoladiya0171@gmail.com>
2023-07-18 11:32:21 +0530

    gan***

---
+++ batsman.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class batsman{
+    private:
+    int bcode, inning, notout, runs, batavg;
+    char bname[20];
+    void calcavg(){
+        batavg=runs/(inning-notout);
+    }
+    public:
+    void readdata(){
+        cout<<"Enter Batsman Code : ";
+        cin>>bcode;
+        cout<<"Enter Batsman name : ";
+        cin>>bname;
+        cout<<"Enter Played Inning : ";
+        cin>>inning;
+        cout<<"Enter Not-Out Inning : ";
+        cin>>notout;
+        cout<<"Enter Batsman Runs : ";
+        cin>>runs;
+        calcavg();
+    }
+    void displaydata(){
+        cout<<"Batsman Code : "<<bcode<<endl;
+        cout<<"Batsman name : "<<bname<<endl;
+        cout<<"Played Inning : "<<inning<<endl;
+        cout<<"Not-Out Inning : "<<notout<<endl;
+        cout<<"Batsman Runs : "<<runs<<endl;
+        cout<<"Average : "<<batavg<<endl;
+    }
+};
+int main(){
+        batsman b;
+
+        b.readdata();
+        b.displaydata();
+    }
---
+++ class.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Test{
+    private:
+    int i;
+
+    public:
+    int a;
+    void hello()
+    {
+        cout<<"Hello"<<endl;
+        i=5;
+    }
+    void getI()
+    {
+        cout<<"value of i: "<<i<<endl;
+    }
+};
+
+int main()
+{
+    Test z;
+    z.a=10;
+    cout<<"value of a: "<<z.a<<endl;
+    z.getI();
+    z.hello();
+    z.getI();
+}
